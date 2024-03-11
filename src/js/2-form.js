4e9eb603414e5dea34764ea93472e14d674d71e1
Korotkyy <118772636+Korotkyy@users.noreply.github.com>
2024-03-11 21:49:06 +0100

    changeshw9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -21,15 +21,19 @@ const loadFormData = () => {
     }
   };
 
-form.addEventListener('input', saveFormData);
-window.addEventListener('load', loadFormData);
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  console.log({
-    email: emailInput.value.trim(),
-    message: textarea.value.trim()
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    if (emailInput.value.trim() !== "" && textarea.value.trim() !== "") {
+      console.log({
+        email: emailInput.value.trim(),
+        message: textarea.value.trim()
+      });
+      
+      localStorage.removeItem(STORAGE_KEY);
+      form.reset();
+    } else {
+      alert("Будь ласка, заповніть всі поля форми.");
+    }
   });
-  localStorage.removeItem(STORAGE_KEY);
-  form.reset();
-});
+  
