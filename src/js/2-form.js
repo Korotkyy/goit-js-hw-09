817d86b7ed6ce53c858de4b80698eea43afaf1c5
Korotkyy <118772636+Korotkyy@users.noreply.github.com>
2024-03-12 18:17:32 +0100

    update 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,39 +3,40 @@ const form = document.querySelector('.feedback-form');
 const textarea = form.querySelector("textarea");
 const emailInput = form.querySelector("input[name='email']");
 
-const saveFormData = () => localStorage.setItem(STORAGE_KEY, JSON.stringify({
-  email: emailInput.value.trim(),
-  message: textarea.value.trim()
-}));
-
 const loadFormData = () => {
     try {
-      const savedData = localStorage.getItem(STORAGE_KEY) ?? "";
-      if (savedData) {
-        const { email, message } = JSON.parse(savedData);
-        emailInput.value = email;
-        textarea.value = message;
-      }
+        const savedData = localStorage.getItem(STORAGE_KEY) ?? "";
+        if (savedData) {
+            const { email, message } = JSON.parse(savedData);
+            emailInput.value = email;
+            textarea.value = message;
+        }
     } catch (error) {
-      console.error("Error loading form data:", error);
+        console.error("Error loading form data:", error);
     }
-  };
-  loadFormData();
+};
 
-  form.addEventListener('submit', (event) => {
+loadFormData(); // Викликаємо функцію при завантаженні сторінки
+
+const saveFormData = () => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({
+        email: emailInput.value.trim(),
+        message: textarea.value.trim()
+    }));
+};
+
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     
     if (emailInput.value.trim() !== "" && textarea.value.trim() !== "") {
-      console.log({
-        email: emailInput.value.trim(),
-        message: textarea.value.trim()
-      });
+        console.log({
+            email: emailInput.value.trim(),
+            message: textarea.value.trim()
+        });
       
-      localStorage.removeItem(STORAGE_KEY);
-      form.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        form.reset();
     } else {
-      alert("Будь ласка, заповніть всі поля форми.");
+        alert("Будь ласка, заповніть всі поля форми.");
     }
-  });
-
-  
+});
